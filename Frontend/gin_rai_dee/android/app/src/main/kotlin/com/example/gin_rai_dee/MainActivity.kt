be6bad1af3eb8035c6eac6d036c55a88f82e8407
Nathamon <s6401012620200@email.kmutnb.ac.th>
2022-08-12 21:38:58 +0700

    version1

--- Frontend/gin_rai_dee/android/app/src/main/kotlin/com/example/gin_rai_dee/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gin_rai_dee
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ Frontend/ginraid/android/app/src/main/kotlin/com/example/ginraid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ginraid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
