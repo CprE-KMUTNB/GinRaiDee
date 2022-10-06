f447fcaa7ddb5c94d62430b7baccb42a84a73141
Nathamon <s6401012620200@email.kmutnb.ac.th>
2022-10-07 02:07:03 +0700

    loginscreen 90%
    เหลือปุ่มเลือก login/signin
    ปรับขนาดปุ่ม login
    ใส่รูปพื้นหลัง

--- Frontend/ginraid/lib/Screens/Login/Login_Screen.dart
+++ Frontend/ginraid/lib/Screens/Login/Login_Screen.dart
@@ -1,125 +1,181 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_new
+
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class loginScreen extends StatelessWidget {
+class loginScreen extends StatefulWidget {
+  const loginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
     var imageStrings = ['Frontend\ginraid\assets\image\loginbacktrans.png'];
+    @override
+    void initState() {
+      var _passwordVisible = false;
+    }
 
     return Scaffold(
-        backgroundColor: Color(0xFFF1F7E7),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                //กินไรดี
-                Text(
-                  'กินไรดีGinrai de',
-                  style: GoogleFonts.sriracha(
-                      fontSize: 50.0, fontStyle: FontStyle.italic),
-                  // style: TextStyle(
-                  //   fontSize: 50.0,
-                  //   fontFamily: "Sriracha",
-                  //   color: Colors.teal
-                  // ),
-                ),
-                SizedBox(height: 40),
+      backgroundColor: Color(0xFFF1F7E7),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              //กินไรดี
+              Text(
+                'กินไรดีGinrai de',
+                style: GoogleFonts.sriracha(
+                    fontSize: 50.0, fontStyle: FontStyle.italic),
+                // style: TextStyle(
+                //   fontSize: 50.0,
+                //   fontFamily: "Sriracha",
+                //   color: Colors.teal
+                // ),
+              ),
+              SizedBox(height: 40),
 
-                //กล่องใหญ่
-                Container(
-                  height: 400,
-                  margin: const EdgeInsets.symmetric(horizontal: 25.0),
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(16)),
-
-                  //กรอกusername
-                  child: 
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: 
-                    TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Color.fromARGB(255, 179, 190, 190)),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        hintText: 'Username',
-                        hintStyle: GoogleFonts.itim(
-                          fontSize: 20.0,
-                          color: Color.fromARGB(255, 179, 190, 190),
-                        ),
-                        labelStyle: GoogleFonts.itim(
-                          fontSize: 30.0,
-                          color: Color.fromARGB(255, 0, 0, 0),
+              //กล่องใหญ่
+              Container(
+                height: 400,
+                margin: const EdgeInsets.symmetric(horizontal: 25.0),
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(16)),
+                child: Column(
+                  children: <Widget>[
+                    //กรอกusername
+                    Container(
+                      child: Container(
+                        child: Center(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 179, 190, 190)),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 0, 0, 0)),
+                              ),
+                              hintText: 'Username',
+                              hintStyle: GoogleFonts.itim(
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 179, 190, 190),
+                              ),
+                              labelStyle: GoogleFonts.itim(
+                                fontSize: 30.0,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
 
-                //   new Row(
+                    SizedBox(height: 50),
 
-                //   //กรอกpass
-                //     child: Padding(
-                //     padding: const EdgeInsets.only(left: 20.0),
-                //     child: TextField(
-                //       obscureText: true,
-                //       decoration: InputDecoration(
-                //         icon: Icon(Icons.visibility),
-                //         suffixIcon: Icon(Icons.remove_red_eye),
-                //         enabledBorder: UnderlineInputBorder(
-                //           borderSide: BorderSide(
-                //               color: Color.fromARGB(255, 179, 190, 190)),
-                //         ),
-                //         focusedBorder: UnderlineInputBorder(
-                //           borderSide:
-                //               BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
-                //         ),
-                //         hintText: 'Password',
-                //         hintStyle: GoogleFonts.itim(
-                //           fontSize: 20.0,
-                //           color: Color.fromARGB(255, 179, 190, 190),
-                //         ),
-                //         labelStyle: GoogleFonts.itim(
-                //           fontSize: 30.0,
-                //           color: Color.fromARGB(255, 0, 0, 0),
-                //         ),
-                //       ),
-                //     ),
-                //   ),
-                //   )
-                // )
+                    //กรอกpass
+                    Container(
+                      child: Container(
+                        child: Center(
+                          child: TextField(
+                            obscureText: _isObscure,
+                            decoration: InputDecoration(
+                              suffixIcon: IconButton(
+                                  icon: Icon(_isObscure
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  }),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 179, 190, 190)),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 0, 0, 0)),
+                              ),
+                              hintText: 'Password',
+                              hintStyle: GoogleFonts.itim(
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 179, 190, 190),
+                              ),
+                              labelStyle: GoogleFonts.itim(
+                                fontSize: 30.0,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
 
-                //login/sigin
+                    //fogetpass
+                    Container(
+                      child: Container(
+                        alignment: Alignment.bottomRight,
+                        child: TextButton(
+                          style: TextButton.styleFrom(),
+                          onPressed: () {},
+                          child: const Text(
+                            'Forget Password',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontFamily: "Sriracha",
+                              color: Color.fromARGB(255, 179, 190, 190),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
 
-                //email
-                // Padding(
-                //   padding: const EdgeInsets.symmetric(horizontal:25.0),
-                //   child: Container(
-                //     decoration: BoxDecoration(
-                //       color:
-                //     ),
-                //     child: TextField(
-                //       decoration : InputDecoration(
-                //         border: InputBorder.none,
-                //       )
-                //     ),
-                //   ),
-                // )
+                    //login/sigin
 
-                //login
-              ],
-            ),
+                    //login
+                    Container(
+                      child: Container(
+                        child: Center(
+                          child: TextButton(                           
+                            style: TextButton.styleFrom(                              
+                              shape: RoundedRectangleBorder(                                
+                                borderRadius: BorderRadius.circular(30),                                
+                              ),                              
+                              backgroundColor: Color.fromARGB(255, 166, 198, 6),                              
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontFamily: "Itim",
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
