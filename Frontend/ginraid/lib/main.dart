4993789262e394bd90f60918826ef6b16edce323
LnWZA00111 <s6401012620226@email.kmutnb.ac.th>
2022-10-18 23:37:41 +0700

    check if user is login
    if userislogin when reopen app => homescreen
    
    if not when reopen app => loginscreen

--- Frontend/ginraid/lib/main.dart
+++ Frontend/ginraid/lib/main.dart
@@ -8,30 +8,54 @@ import 'package:ginraid/Screens/HomeScreen/homeScreen3.dart';
 import 'package:ginraid/Screens/Login/Login_Screen.dart';
 import 'package:ginraid/constant.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/src/material/progress_indicator.dart';
 
 import 'Screens/Signup/Signup_Screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(NotAuthen());
 }
 
-class MyApp extends StatelessWidget {
+class NotAuthen extends StatelessWidget {
   // This widget is the root of your application.
+  Future<Widget> getWidget() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //final success = await prefs.remove('token');
+    String token = await prefs.getString('token') ?? '';
+    if (token == '') {
+      return loginScreen();
+    } else {
+      return homeScreen();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // primaryColor: authenPriColor,
-          scaffoldBackgroundColor: Color(0xFFF1F7E7),
-        ),
-        home: loginScreen());
-        // home: signupScreen());
-        // home: homeScreen());
-        // home: favScreen());
-        //  home: followingScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // primaryColor: authenPriColor,
+        scaffoldBackgroundColor: Color(0xFFF1F7E7),
+      ),
+      home: FutureBuilder(
+        future: getWidget(),
+        builder: (BuildContext context, AsyncSnapshot<Widget> widget) {
+          if (widget.hasData) {
+            return widget.data ?? loginScreen();
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
+  // home: signupScreen());
+  // home: homeScreen());
+  // home: favScreen());
+  //  home: followingScreen());
+
 }
